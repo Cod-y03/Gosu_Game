900e74c64d953f0af0a1ecf8b95326d4d6eccb5f
Cody Mailey <cmailey03@gmail.com>
2019-10-11 10:23:47 -0600

    done with Star Class

--- gosu_game.rb
+++ gosu_game.rb
@@ -1,24 +1,4 @@
 require 'gosu'
-
-class Tutorial < Gosu::Window
-    def initialize
-        super 640, 480
-        self.caption = "Tutorial Game"
-
-        @background_image = Gosu::Image.new("media/space.png", :tileable => true)
-    end
-
-    def update
-        #...
-    end
-
-    def draw
-        @background_image.draw(0,0,0)
-    end
-end
-
-Tutorial.new.show
-
 class Player
     def initialize
         @image = Gosu::Image.new("media/starfighter.bmp")
@@ -27,7 +7,102 @@ class Player
     end
 
     def warp(x, y)
-        @x, @y = x, vel_y
+        @x, @y = x, y
+    end
+
+    def turn_left
+        @angle -= 4.5
     end
 
     def turn_right
+        @angle += 4.5
+    end
+
+    def accelerate
+        @vel_x += Gosu.offset_x(@angle, 0.5)
+        @vel_y += Gosu.offset_y(@angle, 0.5)
+    end
+
+    def move
+        @x += @vel_x
+        @y += @vel_y
+        @x %= 640
+        @y %= 480
+
+        @vel_x *= 0.95
+        @vel_y *= 0.95
+    end
+
+    def draw
+        @image.draw_rot(@x, @y, 1, @angle)
+    end
+end
+
+# ___________________________
+
+module ZOrder
+    BACKGROUND, STARS, PLAYER, UI = *0..3
+end
+
+# ___________________________
+
+class Star
+    attr_reader :x, :y
+
+    def initialize(animation)
+        @animation = animation
+        @color = Gosu::Color::BLACK.dup
+        @color.red = rand(256 - 40) + 40
+        @color.green = rand(256 - 40) + 40
+        @color.blue = rand(256 - 40) + 40
+        @x = rand * 640
+        @y = rand * 480
+        end
+
+        def draw
+            img = @animation[Gosu.milliseconds / 100 % @animation.size]
+            img.draw(@x - img.width / 2.0, @y - img.hight / 2.0,
+                Zorder::STARS, 1, 1, @color, :add)
+        end
+    end
+# ___________________________
+
+
+class Tutorial < Gosu::Window
+    def initialize
+        super 640, 480
+        self.caption = "Tutorial Game"
+
+        @background_image = Gosu::Image.new("media/space.png", :tileable => true)
+
+        @player = Player.new
+        @player.warp(320, 240)
+    end
+
+    def update
+        if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
+            @player.turn_left
+        end
+        if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
+            @player.turn_right
+        end
+        if Gosu.button_down? Gosu::KB_UP or Gosu::button_down? Gosu::GP_BUTTON_0
+            @player.accelerate
+        end
+        @player.move
+    end
+
+    def draw
+        @player.draw
+        @background_image.draw(0,0,0)
+    end
+    def button_down(id)
+        if id == Gosu::KB_ESCAPE
+            close
+        else
+            super
+        end
+    end
+end
+
+Tutorial.new.show
